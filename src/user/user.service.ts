7d70dcfdde633f5b52a6c5814ed59ac7f0f0d5b7
luishidalgoa <luishidalgoa@outlook.es>
2023-12-19 14:19:00 +0100

    mi api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,6 +12,7 @@ export class UserService {
             where: userWhereUniqueInput
         });
     }
+    
     async createUser(data: Prisma.UserCreateInput): Promise<User> {
         return this.prisma.user.create({
             data
